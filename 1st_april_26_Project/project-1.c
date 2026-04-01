dbea9f93370fc11eadcbf53855d5c91dd4a85085
BharatYadav05 <144779835+BharatYadav05@users.noreply.github.com>
2026-04-01 10:24:07 +0530

    Add files via upload

---
+++ 1st_april_26_Project/project-1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+
+    float total, final, a, b, c, gst;
+
+    printf("Enter the Prices of  3 products:\n\n");
+
+    printf("Enter price of 1st product:");
+    scanf("%f", &a);
+    printf("Enter price of 2nd product:");
+    scanf("%f", &b);
+
+    printf("Enter price of 3rd product:");
+    scanf("%f", &c);
+    
+    printf("\n");
+
+    total = a + b + c;
+    gst = (total * 18) / 100;
+    final = total + gst;
+
+    printf("Total Amount : %.2f \n", total);
+    printf("GST (18%) : %.2f \n", gst);
+    printf("Final Amount: %.2f \n", final);
+    printf("Size of Total: %u", sizeof(total));
+
+    // printf("The final amount of purchased 3 products including gst is %.2f \n", final);
+
+    // printf("Size of variable used in this program is :%u\n", sizeof(final));
+
+    return 0;
+}
